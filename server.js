53778ca82f5713f2bf3c2699b58b484c36f283f2
Tupshin Zhu <tup@ad.unc.edu>
2023-01-28 04:08:34 -0500

    fixed errors

--- server.js
+++ server.js
@@ -5,20 +5,14 @@ const http = require('http');
 const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
-var minimist = require('minimist');
+const minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const argv = require('minimalist')(process.argv.slice(2));
+var arg = minimist(process.argv.slice(2));
  
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port;
-if(argv == null){
-	port = 3000;
-}
-else{
-	port = argv;
-}
+const port = arg.port || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
@@ -26,24 +20,25 @@ else{
 // The stuff that should be inside this function is all below.
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
+var x;
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
 	if(err){
 		console.error(err);
 		return;
 	}
-	console.log(data);
-	
+	x = data;
+});
+
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer((req, res) => {
+const server = http.createServer(function(req, res) => {
 	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html");
-	res.end(data);
+	res.setHeader('Content-Type', 'text/html');
+	res.end(x);
 });
 
 
@@ -53,7 +48,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
 	console.log('Server listening on port ${port}');
 });
-});
+
 
 
 // That's it! You're all done!
