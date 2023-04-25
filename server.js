30cfbd5efe591d3b63246aee008c3652cc2ece21
Tupshin Zhu <tup@ad.unc.edu>
2023-04-24 22:13:39 -0400

    server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
-var minimist = require('minimist')
+var minimist = require('minimist');
  
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 var argv = minimist(process.argv.slice(2), {string: "port"});
