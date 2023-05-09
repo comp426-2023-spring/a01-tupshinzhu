29cdf76e5c27bea4e3971fb410cf2da96ddadc54
Tupshin Zhu <tup@ad.unc.edu>
2023-05-09 04:26:41 -0400

    backticks

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const server = http.createServer((req, res) => {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
-	console.log('Server listening on port ${port}');
+	console.log(`Server listening on port ${port}`);
 });
 
 
